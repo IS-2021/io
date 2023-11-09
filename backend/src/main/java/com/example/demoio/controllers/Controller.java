cdf3354853a1e08bc4e83e5d803b7e5a699ed9b5
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-09 21:05:03 +0100

    refactor(backend): Move controllers to /controllers

--- backend/src/main/java/com/example/demoio/controllers/Controller.java
+++ backend/src/main/java/com/example/demoio/controllers/Controller.java
@@ -1,11 +1,13 @@
-package com.example.demoio;
+package com.example.demoio.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -17,7 +19,7 @@ public class Controller {
     @GetMapping("/home")
     public String checkAuth() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(!(auth instanceof AnonymousAuthenticationToken)) {
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
             return "home";
         }
         return "login";
@@ -44,7 +46,7 @@ public class Controller {
     @GetMapping()
     public String defaultPage() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(!(auth instanceof AnonymousAuthenticationToken)) {
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
             return "home";
         }
         return "redirect:login";
@@ -54,6 +56,4 @@ public class Controller {
     public String loginPage() {
         return "login";
     }
-
-
 }
--- backend/src/main/java/com/example/demoio/controllers/RegisterForm.java
+++ backend/src/main/java/com/example/demoio/controllers/RegisterForm.java
@@ -1,5 +1,4 @@
-package com.example.demoio;
-
+package com.example.demoio.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
