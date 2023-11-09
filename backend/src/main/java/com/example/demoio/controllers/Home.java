8fbdbbdadef3fb90e15fc9ee4434cdd5991acde5
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-09 21:21:08 +0100

    refactor(backend): Remove unused poziom1 and references

--- backend/src/main/java/com/example/demoio/controllers/Home.java
+++ backend/src/main/java/com/example/demoio/controllers/Home.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Home extends BaseController {
@@ -16,12 +15,5 @@ public class Home extends BaseController {
             return "home";
         }
         return "login";
-    }
-
-    @PostMapping("/home")
-    public String mainPagePost() {
-        System.out.println("notAuthorized post req");
-
-        return "poziom1";
     }
 }
--- backend/src/main/java/com/example/demoio/controllers/Register.java
+++ backend/src/main/java/com/example/demoio/controllers/Register.java
@@ -2,8 +2,6 @@ package com.example.demoio.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Register {
@@ -11,15 +9,5 @@ public class Register {
     public String handleRegister() {
 
         return "register";
-    }
-
-    @PostMapping("/register")
-    public String registerNewUser(
-            @RequestParam String username,
-            @RequestParam String password
-    ) {
-        System.out.println("notAuthorized post req");
-
-        return "poziom1";
     }
 }
