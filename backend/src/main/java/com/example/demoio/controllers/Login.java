09c0029f68d721f1955ad2a6a02ef68e64d06dc3
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-10 17:10:04 +0100

    feat: Add registration

--- backend/src/main/java/com/example/demoio/controllers/Login.java
+++ backend/src/main/java/com/example/demoio/controllers/Login.java
@@ -1,14 +1,18 @@
 package com.example.demoio.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class Login {
     @GetMapping()
-    public String loginPage() {
+    public String loginPage(@RequestParam(value = "register", required = false) String registerParam, Model model) {
+        model.addAttribute("showRegisteredAlert", registerParam.equals("success"));
+
         return "login";
     }
 }
