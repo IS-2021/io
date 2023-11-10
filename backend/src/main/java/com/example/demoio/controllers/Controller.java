3bb11fc356ba81abca5c4495f9d76fa679c4058a
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-10 17:27:00 +0100

    fix: Fix inconsistent redirects and missing annotation

--- backend/src/main/java/com/example/demoio/controllers/Controller.java
+++ backend/src/main/java/com/example/demoio/controllers/Controller.java
@@ -5,15 +5,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@org.springframework.stereotype.Controller
 public class Controller extends BaseController {
     @GetMapping()
     public String defaultPage() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         if (!(auth instanceof AnonymousAuthenticationToken)) {
-            return "home";
+            return "redirect:/home";
         }
 
-        return "redirect:login";
+        return "redirect:/login";
     }
 }
--- backend/src/main/java/com/example/demoio/controllers/Home.java
+++ backend/src/main/java/com/example/demoio/controllers/Home.java
@@ -1,8 +1,5 @@
 package com.example.demoio.controllers;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Home extends BaseController {
     @GetMapping()
     public String showHomepage() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (!(auth instanceof AnonymousAuthenticationToken)) {
-            return "home";
-        }
-        return "login";
+        return "home";
     }
 }
