741f4f3f97ea16916340f9653e573550f6e7e9e0
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-12-09 00:25:18 +0100

    feat: Add JSON request/response models

--- backend/src/main/java/com/example/demoio/controllers/restapi/User.java
+++ backend/src/main/java/com/example/demoio/controllers/restapi/User.java
@@ -1,7 +1,8 @@
 package com.example.demoio.controllers.restapi;
 
 import com.example.demoio.UserRepository;
-import com.example.demoio.models.restapi.UserData;
+import com.example.demoio.models.dto.UpdateUserCoinsRequest;
+import com.example.demoio.models.dto.UserDataResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,10 +12,10 @@ public class User extends BaseRestApiController {
      * Zwraca ilość monet użytkownika.
      */
     @GetMapping("")
-    public UserData returnUserInfo() {
+    public UserDataResponse returnUserInfo() {
         com.example.demoio.User user = getCurrentUser();
 
-        return new UserData(user.getUsername(), user.getUserCoins());
+        return new UserDataResponse(user.getUsername(), user.getUserCoins());
     }
 
     /**
@@ -23,13 +24,13 @@ public class User extends BaseRestApiController {
      * - user miał 500 monet, w grze zużył 20, więc trzeba tu odesłać 480
      * - user miał 500 monet, w grze zdobył 50, więc trzeba tu odesłać 550
      *
-     * @param userCoins Ilość monet do zapisania
+     * @param userData Obiekt zawierający ilość monet do dodania/odjęcia.
      */
     @PostMapping("/coins")
-    public void updateUserCoins(@RequestBody int userCoins) {
+    public void updateUserCoins(@RequestBody UpdateUserCoinsRequest userData) {
         String username = getCurrentUserName();
         UserRepository userRepository = ctx.getBean(UserRepository.class);
 
-        userRepository.updateUserCoins(userCoins, username);
+        userRepository.updateUserCoins(userData.coins(), username);
     }
 }
