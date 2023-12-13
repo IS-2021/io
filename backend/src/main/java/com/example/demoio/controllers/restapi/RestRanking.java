c8f384b3786468da37bf199f0819c2d6f7350731
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-12-13 23:01:44 +0100

    fix: Add missing @RequestBody, remove coins from saveUserScore

--- backend/src/main/java/com/example/demoio/controllers/restapi/RestRanking.java
+++ backend/src/main/java/com/example/demoio/controllers/restapi/RestRanking.java
@@ -7,6 +7,7 @@ import com.example.demoio.models.dto.UpdateRanking;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestRanking extends BaseRestApiController {
     @Operation(summary = "Zmienia ilość punktów zdobytych przez gracza w danej grze.")
     @PostMapping
-    public void saveUserScore(UpdateRanking updateData) {
+    public void saveUserScore(@RequestBody UpdateRanking updateData) {
         String username = getCurrentUserName();
         UserRepository userRepository = ctx.getBean(UserRepository.class);
 
-        userRepository.save(new UserGames(updateData.gameID(), username, updateData.score(), updateData.coins()));
+        // TODO: Remove coins from this object
+        userRepository.save(new UserGames(updateData.gameID(), username, updateData.score(), 0));
     }
 }
--- backend/src/main/java/com/example/demoio/models/dto/UpdateRanking.java
+++ backend/src/main/java/com/example/demoio/models/dto/UpdateRanking.java
@@ -1,4 +1,4 @@
 package com.example.demoio.models.dto;
 
-public record UpdateRanking(int gameID, int coins, double score) {
+public record UpdateRanking(int gameID, double score) {
 }
